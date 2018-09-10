ddc987de11fdbd6fcfe9493dc300e2d2d9b7e067
Jeremy Zang <zang.jeremy@gmail.com>
2018-09-10 10:39:17 -0400

    Added starter JS for onClick action

---
+++ connectFour.js
@@ -0,0 +1,9 @@
+
+function columnClicked(columnName) {
+	//get the column clicked.
+	var column = document.getElementByClassName("grid-item")
+	//get the element from the DOM
+	document.getElementById(columnName)
+
+	//change the grit item's image to the image supplied 
+}
